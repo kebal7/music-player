1e5ee1b8d5011f7176af6c61a846e24b5b1a5bb7
kebal <kebal.badal555@gmail.com>
2025-08-23 12:58:19 +0545

    Implement: Toggle Pause/Play (UI only)

--- src/main.py
+++ src/main.py
@@ -19,14 +19,25 @@ prev_btn_image = PhotoImage(file=os.path.join(image_dir, "previous.png"))
 control_frame = Frame(root)
 control_frame.pack()
 
-play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
-pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
+# Toggle function
+def toggle_play():
+    if play_btn.winfo_ismapped():
+        play_btn.grid_remove()
+        pause_btn.grid(row=0, column=1, padx=7, pady=10)
+    else:
+        pause_btn.grid_remove()
+        play_btn.grid(row=0, column=1, padx=7, pady=10)
+
+play_btn = Button(control_frame, image=play_btn_image, borderwidth=0,command=toggle_play)
+pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0, command=toggle_play)
 next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
 prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0)
 
 play_btn.grid(row=0, column=1, padx=7, pady = 10)
-pause_btn.grid(row=0, column=2, padx=7, pady = 10)
-next_btn.grid(row=0, column=3, padx=7, pady = 10)
+pause_btn.grid(row=0, column=1, padx=7, pady = 10)
+next_btn.grid(row=0, column=2, padx=7, pady = 10)
 prev_btn.grid(row=0, column=0, padx=7, pady = 10)
+
+pause_btn.grid_remove() #Initially Hidden
 
 root.mainloop()
