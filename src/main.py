efef9335066374d42de4fea19494342bc3d019dc
kebal <kebal.badal555@gmail.com>
2025-08-23 12:40:22 +0545

    Create: Music List, Control Frame

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
 from tkinter import *
+import os
 
 root = Tk()
 root.title('Music Player')
-root.geometry("500x300")
+root.geometry("550x350")
+
+songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
+songlist.pack()
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(script_dir, "..", "resources", "image", "icon")
+
+play_btn_image = PhotoImage(file=os.path.join(image_dir, "play.png"))
+pause_btn_image = PhotoImage(file=os.path.join(image_dir, "pause.png"))
+next_btn_image = PhotoImage(file=os.path.join(image_dir, "next.png"))
+prev_btn_image = PhotoImage(file=os.path.join(image_dir, "previous.png"))
+
+control_frame = Frame(root)
+control_frame.pack()
+
+play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
+pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
+next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
+prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0)
+
+play_btn.grid(row=0, column=1, padx=7, pady = 10)
+pause_btn.grid(row=0, column=2, padx=7, pady = 10)
+next_btn.grid(row=0, column=3, padx=7, pady = 10)
+prev_btn.grid(row=0, column=0, padx=7, pady = 10)
 
 root.mainloop()
